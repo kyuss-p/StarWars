411864f0d37bbc49dda7d1f7a1052fff0756086c
Kyuss <kyuss.poelmans@icloud.com>
2026-03-23 16:57:24 +0100

    Program aangepast om alle huidige klasses en abilities te testen

--- Program.cs
+++ Program.cs
@@ -4,19 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Jedi lukeSkywalker = new Jedi(75, 60, "Luke Skywalker", 50);
-            Sith darthVader = new Sith(85, 75, "Darth Vader", 60);
+            Jedi anakinSkywalker = new Jedi(75, 70, "Anakin Skywalker", 60);
+            Padawan ahsokaTano = new Padawan(60, 45, "Ahsoka Tano", 30, anakinSkywalker);
+            Sith countDooku = new Sith(70, 80, "Count Dooku", 75);
+            SithApprentice savageOpress = new SithApprentice(65, 55, "Savage Opress", 60, countDooku);
+            Trooper captainRex = new Trooper(55, 45, "Captain Rex", "CT-7567");
+            BountyHunter jangoFett = new BountyHunter(60, 50, "Jango Fett");
 
-            darthVader.Attack(lukeSkywalker);
-            lukeSkywalker.Attack(darthVader);
-            darthVader.ForceAbility(lukeSkywalker);
-            darthVader.Attack(lukeSkywalker);
+            ahsokaTano.ForceAbility(countDooku);
+            anakinSkywalker.Attack(countDooku);
+            countDooku.ForceAbility(anakinSkywalker);
+            savageOpress.ForceAbility(ahsokaTano);
+            captainRex.Attack(countDooku);
+            captainRex.SpecialAbility(savageOpress);
+            jangoFett.SpecialAbility(captainRex);
+            jangoFett.Reload();
+            jangoFett.Attack(captainRex);
 
             Console.WriteLine();
 
-            Console.WriteLine(lukeSkywalker);
-            Console.WriteLine(darthVader);
-
+            Console.WriteLine(anakinSkywalker);
+            Console.WriteLine(ahsokaTano);
+            Console.WriteLine(countDooku);
+            Console.WriteLine(savageOpress);
+            Console.WriteLine(captainRex);
+            Console.WriteLine(jangoFett);
         }
     }
 }
