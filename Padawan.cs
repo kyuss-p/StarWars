c45e0d1a04a084f7bc0090715ed47a421c05a41e
Kyuss <kyuss.poelmans@icloud.com>
2026-03-23 01:04:02 +0100

    Padawan en SithApprentice subklasse gemaakt

---
+++ Padawan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace StarWars
+{
+    internal class Padawan : Jedi
+    {
+        public Jedi Master { get; set; }
+
+        public Padawan(int attackPower, int defense, string name, int forcePoints, Jedi master) : base(attackPower, defense, name, forcePoints)
+        {
+            Health = 75;
+            Master = master;
+        }
+
+        public override void ForceAbility(Character target)
+        {
+            if (!IsAlive)
+            {
+                return;
+            }
+
+            int cost = 15; 
+            if (cost > ForcePoints)
+            {
+                Console.WriteLine("Not enough Force Points to use Force Push");
+                return;
+            }
+            ForcePoints -= cost;
+
+            int damage = (int)(10 * (1 + ForcePoints / 100.0));
+            Console.WriteLine($"{Name} used Force Push on {target.Name} and deals {damage} damage");
+            target.TakeDamage(damage);
+            target.Defense = Math.Max(0, target.Defense - 10);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Master: {Master.Name}";
+        }
+    }
+}
---
+++ SithApprentice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace StarWars
+{
+    internal class SithApprentice : Sith
+    {
+        public Sith Master { get; set; }
+
+        public SithApprentice(int attackPower, int defense, string name, int rageLevel, Sith master) : base(attackPower, defense, name, rageLevel)
+        {
+            Health = 80;
+            Master = master;
+        }
+
+        public override void ForceAbility(Character target)
+        {
+            if (!IsAlive)
+            {
+                return;
+            }
+
+            int cost = 15;
+            if (cost > RageLevel)
+            {
+                Console.WriteLine("Rage Level not high enough to use Force Drain");
+                return;
+            }
+            RageLevel -= cost;
+
+            int healthBefore = Health;
+            int baseDamage = 12;
+            int damage = (int)(baseDamage * (1 + (RageLevel / 100.0)));
+            Health = Math.Min(100, Health + damage);
+            int actualHeal = Health - healthBefore;
+
+            Console.WriteLine($"{Name} used Force Drain on {target.Name} dealing {damage} damage and gaining {damage} HP");
+            target.TakeDamage(damage);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Master: {Master.Name}";
+        }
+    }
+}
