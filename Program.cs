a1f8b7a5a4f4fdf7c27998cbaf3761c0085e52d9
Kyuss <kyuss.poelmans@icloud.com>
2026-03-25 10:32:14 +0100

    Program aangepast om nieuwe klasses te testen

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@
             SithApprentice savageOpress = new SithApprentice(65, 55, "Savage Opress", 60, countDooku);
             Trooper captainRex = new Trooper(55, 45, "Captain Rex", "CT-7567");
             BountyHunter jangoFett = new BountyHunter(60, 50, "Jango Fett");
+            BattleDroid b1BattleDroid = new BattleDroid(50, 30, "B1 Battle Droid", "B1");
+            AstroDroid r2d2 = new AstroDroid(30, 20, "R2-D2", "R2");
 
             ahsokaTano.ForceAbility(countDooku);
             anakinSkywalker.Attack(countDooku);
@@ -20,6 +22,8 @@
             jangoFett.SpecialAbility(captainRex);
             jangoFett.Reload();
             jangoFett.Attack(captainRex);
+            b1BattleDroid.DroidAbility(anakinSkywalker);
+            r2d2.DroidAbility(b1BattleDroid);
 
             Console.WriteLine();
 
@@ -29,6 +33,8 @@
             Console.WriteLine(savageOpress);
             Console.WriteLine(captainRex);
             Console.WriteLine(jangoFett);
+            Console.WriteLine(b1BattleDroid);
+            Console.WriteLine(r2d2);
         }
     }
 }
