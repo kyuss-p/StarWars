9b74d5a6cd2fd00e47250eb6c1ad0ea43bb2dc18
Kyuss <kyuss.poelmans@icloud.com>
2026-03-25 10:30:36 +0100

    AstroDroid klasse aangemaakt

---
+++ AstroDroid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWars
+{
+    public class AstroDroid : Droid
+    {
+        public string Unit { get; set; }
+
+        public AstroDroid(int attackPower, int defense, string name, string unit) : base(attackPower, defense, name)
+        {
+            Unit = unit;
+        }
+
+        public override void DroidAbility(Character target)
+        {
+            if (!IsAlive)
+            {
+                return;
+            }
+
+            int cost = 25;
+            if (cost > BatteryLevel)
+            {
+                Console.WriteLine($"{Name} does not have enough battery to use Shock");
+                return;
+            }
+            BatteryLevel -= cost;
+
+            int newDefense = target.Defense / 2;
+            Console.WriteLine($"{Name} uses Shock on {target.Name} and lowers his defense to {newDefense}");
+            target.Defense = newDefense;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Unit: {Unit}";
+        }
+    }
+}
